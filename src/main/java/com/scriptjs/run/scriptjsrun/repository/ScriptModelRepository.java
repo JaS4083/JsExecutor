b78978de745eeb9834f92b189207fb2e950106dd
Artem Sirobaba <sirobabaartem@gmail.com>
2020-08-29 19:12:19 +0300

    small fix

--- src/main/java/com/scriptjs/run/scriptjsrun/repository/ScriptModelRepository.java
+++ src/main/java/com/scriptjs/run/scriptjsrun/repository/ScriptModelRepository.java
@@ -3,5 +3,4 @@ package com.scriptjs.run.scriptjsrun.repository;
 import com.scriptjs.run.scriptjsrun.model.ScriptModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ScriptModelRepository extends MongoRepository<ScriptModel, String> {
-}
+public interface ScriptModelRepository extends MongoRepository<ScriptModel, String> {}
